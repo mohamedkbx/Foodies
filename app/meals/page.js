dcd39bb279ba2963a9e96afedbceb26203c2d25a
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com.>
2025-07-16 12:35:19 +0300

    feat: implement MealsPage layout with header and meals grid component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
 export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+  return <>
+  <header className={classes.header}>
+    <h1>Delicious Meals <span className={classes.highlight}>by you</span></h1>
+    <p>choose your favorite Meal and cook it with your self</p>
+    <p className={classes.cta}>
+      <Link href="/meals/share"> Share your favorite Recipe</Link>
+    </p>
+  </header>
+  <main className={classes.main}>
+    <MealsGrid meals={[]}/>
+  </main>
+  </>
 }
