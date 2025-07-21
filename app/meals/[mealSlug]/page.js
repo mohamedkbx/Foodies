dfebfcebb2e6ec9b204ab8f69bdd8e6dd493d28a
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com.>
2025-07-21 10:49:27 +0300

    feat: implement meal details page with dynamic data fetching and improved layout

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,23 +1,34 @@
 import Image from 'next/image';
 import classes from './page.module.css';
-export default function MealDetailsPage() {
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
+export default  function MealDetailsPage({params}) {
+
+  const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
+
   return <>
   <header className={classes.header}>
     <div className={classes.image}>
-      <Image fill />
+      <Image src={meal.image} alt={meal.title} fill />
     </div>
     <div className={classes.headerText}>
-      <h1>Meal Details</h1>
+      <h1>{meal.title}</h1>
       <p className={classes.creator}>
-        by <a href="#">Chef Name</a>
+        by <a href="#">{meal.creator}</a>
       </p>
       <p className={classes.summary}>
-        Summary
+        {meal.summary}
       </p>
     </div>
   </header>
   <main>
-    <p className={classes.instructions} dangerouslySetInnerHTML={}></p>
+    <p className={classes.instructions} dangerouslySetInnerHTML={{ 
+      __html:meal.instructions.replace(/\n/g, '<br />')
+    }}></p>
   </main>
   </>
 }
